071a44c64f3a614c3dfedae7eeb0954d5d7600be
stchloe <chloestuart11@gmail.com>
2024-03-19 17:22:25 +0000

    Edge Detection & Cropping applied
    Moved test images into separate folder. Used thresholding to create binary image and Canny edge detection to crop image to size of petri dish.

--- DataCleaning/preprocessImage/findCircles.py
+++ DataCleaning/preprocessImage/findCircles.py
@@ -1,28 +1,42 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-# Read image.
-img = cv2.imread(r'C:\TeamProject\Automated-Plate-Analysis\DataCleaning\SampleImage001.jpg', cv2.IMREAD_COLOR)
+# Read image
+img = cv2.imread(r"C:\TeamProject\Automated-Plate-Analysis\DataCleaning\met0_125.jpg",
+                 cv2.IMREAD_REDUCED_GRAYSCALE_4)
 
-# Convert to grayscale.
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Convert BGR to RGB
+img_rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-# set the kernel size, depending on whether we are using the Sobel
-# operator of the Scharr operator, then compute the gradients along
-# the x and y-axis, respectively
-ksize = 3
-gX = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize)
-gY = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=ksize)
-# the gradient magnitude images are now of the floating point data
-# type, so we need to take care to convert them back a to unsigned
-# 8-bit integer representation so other OpenCV functions can operate
-# on them and visualize them
-gX = cv2.convertScaleAbs(gX)
-gY = cv2.convertScaleAbs(gY)
-# combine the gradient representations into a single image
-combined = cv2.addWeighted(gX, 0.5, gY, 0.5, 0)
+# Apply thresholding to convert to binary
+ret, thresh = cv2.threshold(img, 70, 255, 0)
 
-cv2.imshow('Sobel/Scharr', combined)
-cv2.imwrite('sobelScharr.jpg', combined)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Apply Canny edge detection
+edges = cv2.Canny(thresh, threshold1=150, threshold2=200)
+
+# find the non-zero min-max coords of canny
+pts = np.argwhere(edges > 0)
+y1, x1 = pts.min(axis=0)
+y2, x2 = pts.max(axis=0)
+
+# crop the region
+cropped = edges[y1:y2, x1:x2]
+cv2.imwrite("cropped.png", cropped)
+
+# Find contours
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Draw contours on the original image
+cv2.drawContours(img_rgb, contours, -1, (0, 255, 0), 3)
+
+# Draw rectangle around original image
+tagged = cv2.rectangle(img_rgb.copy(), (x1, y1), (x2, y2), (255, 0, 0), 3, cv2.LINE_AA)
+cv2.imshow("tagged", tagged)
+cv2.waitKey()
+
+plt.subplot(121), plt.imshow(tagged ), plt.title('Greyscale Image with Bounding Box and Contours')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(cropped, cmap='gray'), plt.title('Binary Cropped Image')
+plt.xticks([]), plt.yticks([])
+plt.show()
